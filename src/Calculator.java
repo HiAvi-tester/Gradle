5c3822b5912800ee5cfe953a3f5959329eba4c56
HiAvi-tester <47265288+HiAvi-tester@users.noreply.github.com>
2020-05-20 14:03:57 +0530

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
 public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);
+Scanner reader = new Scanner(System.in)
 System.out.print("Enter two numbers: ");
 // nextDouble() reads the next double from the keyboard
 double first = reader.nextDouble();
